40e65c38cdf0e70d9f05bf3db7222e7ba626c7e2
toran billups <toranb@gmail.com>
2017-04-15 19:52:06 -0500

    finally: the broccoli + flow magic to transpile this

--- index.js
+++ index.js
@@ -3,12 +3,23 @@
 
 const mergeTrees = require('broccoli-merge-trees');
 const path = require('path');
+const esTranspiler = require('broccoli-babel-transpiler');
 
 module.exports = {
   name: 'redux-offline',
   treeForAddon (tree) {
     const offlinePath = path.dirname(require.resolve('redux-offline/src/index.js'));
-    let offlineTree = this.treeGenerator(offlinePath);
+
+    var offlineTree = esTranspiler(offlinePath, {
+      plugins: [
+        'transform-object-rest-spread',
+        'transform-class-properties',
+        'transform-flow-strip-types'
+      ],
+      presets: [
+          ['latest', 'react']
+      ]
+    });
 
     if (!tree) {
       return this._super.treeForAddon.call(this, offlineTree);

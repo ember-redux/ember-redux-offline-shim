4ce74d8059459337ed48cddd1e17c2efee76a911
toran billups <toranb@gmail.com>
2017-04-17 19:00:53 -0500

    updated readme/ removed non working deps from blueprint

--- blueprints/ember-redux-offline-shim/index.js
+++ blueprints/ember-redux-offline-shim/index.js
@@ -5,8 +5,7 @@ module.exports = {
       {name: 'babel-plugin-transform-class-properties', target: '6.24.1'},
       {name: 'babel-plugin-transform-object-rest-spread', target: '6.23.0'},
       {name: 'babel-plugin-transform-flow-strip-types', target: '6.22.0'},
-      {name: 'babel-preset-latest', target: '6.24.1'},
-      {name: 'ember-redux-persist-shim', target: '0.0.3'}
+      {name: 'babel-preset-latest', target: '6.24.1'}
     ])
   },
 
